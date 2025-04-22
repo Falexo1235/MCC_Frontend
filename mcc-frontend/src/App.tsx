0b25307249ca5c4992c09aaaab04f6e5d2902085
Anisimov Aleksey <fallengalaxy5000@gmail.com>
2025-04-22 14:37:38 +0700

    Add nodes
    Добавление узлов, функция отмены выделения при нажатия на фон списка узлов

--- mcc-frontend/src/App.tsx
+++ mcc-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import TreeView from './components/TreeView'
 import initialData from "./data.json"
@@ -15,7 +15,19 @@ const App = () => {
   const [currentRootId, setCurrentRootId] = useState<string | null>(null)
   const [selectedNode, setSelectedNode] = useState<string | null>(null)
   const handleAdd = () => {
+    if (selectedNode) {
+      handleAddChild(selectedNode)
+    } else { handleAddChild(null) }
+  }
 
+  const handleAddChild = (parentId: string | null) => {
+    const newId = (Math.max(...treeData.map((node) => Number.parseInt(node.id)), 0) + 1).toString()
+    const newNode: TreeNode = {
+      id: newId,
+      text: `Node ${newId}`,
+      parent: parentId
+    }
+    setTreeData([...treeData, newNode])
   }
   const handleEdit = () => {
     
@@ -38,12 +50,20 @@ const App = () => {
   }
   const handleReset = () => {
     setTreeData(initialData.objects)
+    handleDeselect()
   }
+
+  const handleDeselect = () => {
+    if(selectedNode){
+      setSelectedNode(null)
+    }
+  }
+
   return (
-    <div className='app-container'>
+    <div className='app-container' >
       <h1>Дерево</h1>
       <div className='tree-container'>
-        <div className='main-tree'>
+        <div className='main-tree'onClick={handleDeselect} >
           <TreeView
           data={treeData}
           rootId={currentRootId}
